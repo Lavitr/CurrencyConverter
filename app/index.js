b813fda065301ef34c29b5427fed84418738e1a4
Ron Lavit <ronlavit@Rons-MacBook-Pro.local>
2019-07-06 17:46:35 +0300

    Text input

--- app/index.js
+++ app/index.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import {
-//   StyleSheet, Text, View, Button, Alert,
-// } from 'react-native';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-// export default function App() {
-//   const onPressLearnMore = () => {
-//     console.log('WWWW');
-//     Alert.alert('yes');
-//   };
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.from index</Text>
-//       <Text>Open up App.from index</Text>
-//       <Button
-//         onPress={onPressLearnMore}
-//         title="Learn More!!!!"
-//         color="#841584"
-//         accessibilityLabel="Learn more about this purple button"
-//       />
-//     </View>
-//   );
-// }
-
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
@@ -40,6 +6,9 @@ import Home from './screens/Home';
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
+  $border: '#979797',
+  $inputText: '#797979',
 });
 
 export default () => <Home />;
