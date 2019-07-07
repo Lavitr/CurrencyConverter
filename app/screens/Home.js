dd5c53821e45c7075b3c46e44757e80db26b122a
Ron Lavit <ronlavit@Rons-MacBook-Pro.local>
2019-07-07 15:18:05 +0300

    Clear button and styled text added

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,13 +4,18 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_LAST_CONVERTED = new Date();
+const TEMP_CONVERSION_RATE = 0.79739;
 
 class Home extends Component {
+  
   handleChangeText = () => {
     console.log('change text !!!!');
   };
@@ -21,6 +26,14 @@ class Home extends Component {
 
   handlePressQuoteCurrency = () => {
     console.log('press quote currency');
+  };
+
+  handleSwapCurrency = () => {
+    console.log('handle swap currency');
+  };
+
+  handleSwapCurrency = () => {
+    console.log('handle swap currency');
   };
 
   render() {
@@ -41,6 +54,13 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           value={TEMP_QUOTE_PRICE}
         />
+        <LastConverted
+          date={TEMP_LAST_CONVERTED}
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
+        />
+        <ClearButton onPress={this.handleSwapCurrency} text="Reverse Currencies" />
       </Container>
     );
   }
