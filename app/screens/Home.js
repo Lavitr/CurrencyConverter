1563c2276cbd2a66c8849389eff78d80aeb863e1
Ron Lavit <ronlavit@Rons-MacBook-Pro.local>
2019-07-09 23:34:43 +0300

    Avoiding the KeyBoard

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar, Alert } from 'react-native';
+import { StatusBar, KeyboardAvoidingView} from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
@@ -46,6 +46,7 @@ class Home extends Component {
       <Container>
         <StatusBar backgroundColor="blue" barStyle="light-content" />
         <Header onPress={this.handleOptionsPress} />
+        <KeyboardAvoidingView behavior="padding">
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
@@ -67,6 +68,7 @@ class Home extends Component {
           conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton onPress={this.handleSwapCurrency} text="Reverse Currencies" />
+        </KeyboardAvoidingView>
       </Container>
     );
   }
